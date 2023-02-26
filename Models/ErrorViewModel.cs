25b459e2a5b1a0dfb878ab2c88790be423f826e5
simo-sw <simonecapelli13@gmail.com>
2023-02-26 10:53:17 +0100

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProvaCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
